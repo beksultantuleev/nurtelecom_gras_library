df3021842493f980f68a283a5a97791adb256c64
Bekskultan Tuleev <btuleev@nurtelecom.kg>
2022-11-25 11:04:47 +0600

    com

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
         'Development Status :: 3 - Alpha',
         # Define that your audience are developers
-        'Intended Audience :: Developers of Nurtelecom',
+        'Intended Audience :: Developers',
         'Topic :: Software Development :: Build Tools',
         'License :: OSI Approved :: MIT License',   # Again, pick a license
         # Specify which pyhton versions that you want to support
