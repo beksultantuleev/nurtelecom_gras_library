c315dc5fe1cca906a4057ddc2816880c57747027
Bekskultan Tuleev <btuleev@nurtelecom.kg>
2023-01-27 09:30:04 +0600

    com

--- src/nurtelecom_gras_library/__init__.py
+++ src/nurtelecom_gras_library/__init__.py
@@ -3,4 +3,4 @@ __version__ = version("nurtelecom_gras_library")
 
 from nurtelecom_gras_library.additional_functions import *
 from nurtelecom_gras_library.PLSQL_data_importer import PLSQL_data_importer
-from nurtelecom_gras_library.PLSQL_geodata_importer import PLSQL_geodata_importer
+# from nurtelecom_gras_library.PLSQL_geodata_importer import PLSQL_geodata_importer
