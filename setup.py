d3a22a4b8c017e19858ac860e28ae565af93544a
Beksultan Tuleev <kazamabeks@gmail.com>
2023-05-01 11:28:42 +0200

    readme

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
-
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
 
 setup(
     name='nurtelecom_gras_library',
-    version='1.1.3',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version='1.1.4',
     license='MIT',
     author="Beksultan Tuleev",
     author_email='kazamabeks@gmail.com',
