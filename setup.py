a4199c5a9860ab09f342640daae23e32fd9d7a05
Beksultan Tuleev <kazamabeks@gmail.com>
2023-05-01 11:40:04 +0200

    updated readme

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='nurtelecom_gras_library',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.1.4',
+    version='1.1.5',
     license='MIT',
     author="Beksultan Tuleev",
     author_email='kazamabeks@gmail.com',
