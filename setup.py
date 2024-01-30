4aeeef51bfe3a6823bc03a5b4529fb9828f8ba41
Beksultan Tuleev <btuleev@nurtelecom.kg>
2024-01-30 09:46:25 +0600

    updated version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='nurtelecom_gras_library',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.2.2',
+    version='1.2.3',
     license='MIT',
     author="Beksultan Tuleev",
     author_email='kazamabeks@gmail.com',
