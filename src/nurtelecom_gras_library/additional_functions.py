9eeaae0f8680e276b76aec94088ba27211ad4449
Beksultan Tuleev <btuleev@nurtelecom.kg>
2024-03-12 15:05:37 +0600

    bugs fixed

--- src/nurtelecom_gras_library/additional_functions.py
+++ src/nurtelecom_gras_library/additional_functions.py
@@ -9,7 +9,6 @@ from email.policy import SMTP
 import timeit
 import requests
 import base64
-from PLSQL_data_importer import PLSQL_data_importer
 
 def insert_from_pandas(data, counter, list_of_column_names, full_data_length=None):
     '''
@@ -273,19 +272,7 @@ def voronoi_split(poly_figure, coords_list, buffer_value=2000, boundary_value=10
 
     return result
 
-def get_db_connection(user, database, port='1521'):
-    database = database.upper()
-    user = user.upper()
-    database_connection = PLSQL_data_importer(user=user,
-                                              password=pass_decoder(
-                                                  os.environ.get(f'{user}_{database}')),
-                                              host=pass_decoder(
-                                                  os.environ.get(f'{database}_IP')),
-                                              service_name=pass_decoder(
-                                                  os.environ.get(f'{database}_SERVICE_NAME')),
-                                              port=port
-                                              )
-    return database_connection
+
 
 if __name__ == "__main__":
     # database_connection = get_db_connection('kpi', 'dwh_sd')
